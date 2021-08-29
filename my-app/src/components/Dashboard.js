980b7be0930218fb8c6e8d20a212d9e8db283806
samchsu <63684737+samchsu@users.noreply.github.com>
2021-08-29 15:53:19 -0700

    Obtained top artists and tracks for a specific user

--- my-app/src/components/Dashboard.js
+++ my-app/src/components/Dashboard.js
@@ -12,7 +12,7 @@ function Dashboard() {
         fetch("http://localhost:3000/auth", {
             method: "POST",
             headers: {
-            "Content-Type": "application/json",
+                "Content-Type": "application/json",
                 Accept: "application/json"
             },
             body: JSON.stringify({
@@ -28,11 +28,63 @@ function Dashboard() {
         })
         console.log(accessToken);
     }, []);
-    
+    console.log(token)
+
+    async function topTracksL() {
+        await fetch ("https://api.spotify.com/v1/me/top/tracks?time_range=long_term", {
+            method: "GET",
+            headers: {
+                'Authorization': 'Bearer ' + token,
+                "Content-Type": "application/json",
+                Accept: "application/json"
+            }
+        })
+        .then (res => res.json())
+        .then (res => {
+            console.log(res);
+        })
+    }
+
+    async function topTracksM() {
+        await fetch ("https://api.spotify.com/v1/me/top/tracks?time_range=medium_term", {
+            method: "GET",
+            headers: {
+                'Authorization': 'Bearer ' + token,
+                "Content-Type": "application/json",
+                Accept: "application/json"
+            }
+        })
+        .then (res => res.json())
+        .then (res => {
+            console.log(res);
+        })
+    }
+
+    async function topTracksS() {
+        await fetch ("https://api.spotify.com/v1/me/top/tracks?time_range=short_term", {
+            method: "GET",
+            headers: {
+                'Authorization': 'Bearer ' + token,
+                "Content-Type": "application/json",
+                Accept: "application/json"
+            }
+        })
+        .then (res => res.json())
+        .then (res => {
+            console.log(res);
+        })
+    }
+
     return (
       <div className="Dashboard">
           <div className="text-container">
               {token}
+              <br></br>
+              <button onClick={topTracksL}>all time</button>
+              <button onClick={topTracksM}>last 6 months</button>
+              <button onClick={topTracksS}>last month</button>
+              <br></br>
+              <a href="http://localhost:3001" class="btn btn-primary">Go back</a>
           </div>
       </div>
   )
